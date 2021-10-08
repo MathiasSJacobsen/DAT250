5a30c32f7ba15f5503b62604536b87abdb9ab55f
Mathias <mathias.skallerud@gmail.com>
2021-10-08 08:31:58 +0200

    make the example

--- expass2-code/banking/src/main/java/Main.java
+++ expass2-code/banking/src/main/java/Main.java
@@ -16,5 +16,58 @@ public class Main {
         List<Person> personList = q.getResultList();
         personList.forEach(System.out::println);
 
+        em.getTransaction().begin();
+        Bank bank = new Bank();
+        bank.setName("Pengebank");
+        em.persist(bank);
+
+        Pincode pincode = new Pincode();
+        pincode.setPincode(123);
+        pincode.setCount(1);
+
+        CreditCard creditCard1 = new CreditCard();
+        creditCard1.setNumber(12345L);
+        creditCard1.setBalance((float) -5000.0);
+        creditCard1.setLimit((float) -10000.0);
+        creditCard1.setBank(bank);
+        creditCard1.setPincode(pincode);
+        // creditCard.setOwner();
+        em.persist(creditCard1);
+
+        CreditCard creditCard2 = new CreditCard();
+        creditCard2.setBank(bank);
+        creditCard2.setPincode(pincode);
+        creditCard2.setNumber(123L);
+        creditCard2.setBalance((float) 1.0);
+        creditCard2.setLimit(2000);
+        em.persist(creditCard2);
+
+
+        Address address = new Address();
+        address.setNumber(28);
+        address.setStreet("Inndalsveien");
+        em.persist(address);
+
+        Person person = new Person();
+        person.setName("Max Mustermann");
+        person.setAddress(new ArrayList<>(Collections.singleton(address)));
+        person.setCreditCardList(new ArrayList<>(Collections.singleton(creditCard1)));
+        person.getCreditCardList().add(creditCard2);
+        em.persist(person);
+
+        creditCard1.setOwner(person);
+        em.merge(creditCard1);
+        creditCard2.setOwner(person);
+        bank.setCreditCardList(new ArrayList<>(Collections.singleton(creditCard1)));
+        bank.getCreditCardList().add(creditCard2);
+        em.merge(bank);
+        address.setPerson(new ArrayList<>(Collections.singleton(person)));
+        em.merge(address);
+
+        em.getTransaction().commit();
+
+        em.close();
+
+
     }
 }
